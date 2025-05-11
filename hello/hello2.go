cd394c96ad716de3600a0879cb861fe82500d294
Bryan Escobar <bescobarres@gmail.com>
2025-05-10 23:10:24 -0500

    Add more examples of fmt library

--- hello/hello2.go
+++ hello/hello2.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
 
 func main() {
 	fmt.Println("hello world from el mundo del sistema en GO")
+	fmt.Println(quote.Go())
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Glass())
+	fmt.Println(quote.Opt())
+	fmt.Println(quote.Hello())
 }
